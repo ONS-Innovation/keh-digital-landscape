a7e361bff5a821c151a9e0dfb0c5b762bcf7af26
Sebastian <Sebastian.Evison-Theodossiades@ons.gov.uk>
2025-03-12 10:19:24 +0000

    rm useless fields and add stage+developed

--- backend/src/utilities/projectDataTransformer.js
+++ backend/src/utilities/projectDataTransformer.js
@@ -14,7 +14,7 @@ function transformProjectToCSVFormat(project) {
   
   // Get source control details
   const sourceControl = project.source_control[0]?.type || "";
-  
+  const developed = project.developed[1] != '' ? `${project.developed[0]} with ${project.developed.slice(1).join(", ")}` : project.developed[0];
   // Transform to match desired CSV structure
   return {
     Project: project.details[0]?.name || "",
@@ -22,35 +22,19 @@ function transformProjectToCSVFormat(project) {
     Programme: project.details[0]?.programme_name || "",
     Programme_Short: project.details[0]?.programme_short_name || "",
     Description: project.details[0]?.project_description || "",
-    Project_Area: "",
-    DST_Area: "",
+    Stage: project.stage || "",
+    Developed: developed,
     Team: technicalContact,
     Language_Main: mainLanguages,
     Language_Others: otherLanguages,
     Language_Frameworks: frameworks,
-    Testing_Frameworks: "", // Not directly available in new format
     Hosted: project.architecture.hosting.type ? project.architecture.hosting.type.join("; ") : "",
-    Messaging_Type: "",
-    Containers: "", // Not directly available in new format
     Architectures: project.architecture.hosting.details ? project.architecture.hosting.details.join("; ") : "",
     Source_Control: sourceControl,
-    Branching_Strategy: "",
     Repo: project.source_control[0]?.links ? project.source_control[0]?.links.map(link => link.url).join("; ") : "",
-    Static_Analysis: "",
-    Code_Formatter: "",
-    Package_Manager: "",
-    Security_Tools: "",
     CICD: project.architecture.cicd.others ? project.architecture.cicd.others.join("; ") : "",
-    CICD_Orchestration: "",
-    Monitoring: "",
     Datastores: project.architecture.database.others ? project.architecture.database.others.join("; ") : "",
     Database_Technologies: project.architecture.database.main ? project.architecture.database.main.join("; ") : "",
-    Data_Output_Formats: "",
-    Business_Dashboards: "",
-    Integrations_ONS: "",
-    Integrations_External: "",
-    IAM_Services: "",
-    Datasets_Used: "",
     Project_Tools: project.supporting_tools.project_tracking || "",
     Documentation: project.details[0]?.documentation_link?.join("; ") || "",
     Infrastructure: project.architecture.infrastructure.others ? project.architecture.infrastructure.others.join("; ") : "",
