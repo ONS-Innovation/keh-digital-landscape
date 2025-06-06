1af76618f95a2f17feea68999946683c3f393e5d
evisos <Sebastian.Evison-Theodossiades@ons.gov.uk>
2025-06-06 11:29:45 +0100

    KEH-941-PATCH-1 - Add /copilot/api rule to alb (#81)
    * add /copilot/api rule to alb
    
    * add alb rule to main.tf

--- terraform/service/alb.tf
+++ terraform/service/alb.tf
@@ -241,10 +241,32 @@ resource "aws_lb_listener_rule" "tech_radar_admin_backend_rule" {
   }
 }
 
+resource "aws_lb_listener_rule" "digital_landscape_copilot_api_rule" {
+  listener_arn = data.terraform_remote_state.ecs_infrastructure.outputs.application_lb_https_listener_arn
+  priority     = module.alb_listener_priority.highest_priority + 6
+
+  condition {
+    host_header {
+      values = ["${local.service_url}"]
+    }
+  }
+
+  condition {
+    path_pattern {
+      values = ["/copilot/api/*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.backend_new_tg.arn
+  }
+}
+
 # General API access (fourth priority)
 resource "aws_lb_listener_rule" "digital_landscape_api_rule" {
   listener_arn = data.terraform_remote_state.ecs_infrastructure.outputs.application_lb_https_listener_arn
-  priority     = module.alb_listener_priority.highest_priority + 6
+  priority     = module.alb_listener_priority.highest_priority + 7
 
   condition {
     host_header {
@@ -267,7 +289,7 @@ resource "aws_lb_listener_rule" "digital_landscape_api_rule" {
 # General frontend access (lowest priority)
 resource "aws_lb_listener_rule" "digital_landscape_frontend_rule" {
   listener_arn = data.terraform_remote_state.ecs_infrastructure.outputs.application_lb_https_listener_arn
-  priority     = module.alb_listener_priority.highest_priority + 7
+  priority     = module.alb_listener_priority.highest_priority + 8
 
   condition {
     host_header {
