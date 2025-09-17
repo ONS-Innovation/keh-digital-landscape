ef9bafce5e8945cc4166fac304ce96521d51ecfa
Anthony Njonge <47349801+mwirikia@users.noreply.github.com>
2025-09-17 18:16:10 +0100

    Add test

--- testing/frontend/tests/data/cloudBlipCases.js
+++ testing/frontend/tests/data/cloudBlipCases.js
@@ -1,25 +1,40 @@
 // Data for cloud blips in the tech radar
 export const cloudBlipCases = [
-    {
-      name: 'AWS',
-      blipNumber: '1',
-      heading: 'AWS',
-      projects: [
-        'AWS Project',
-        'Another Project - GCP and AWS services',
-        'AWS with GCP services',
-      ],
-      projectsCountText: '3 projects',
-    },
-    {
-      name: 'GCP',
-      blipNumber: '2',
-      heading: 'GCP',
-      projects: [
-        'GCP Project',
-        'Another Project - GCP and AWS services',
-        'AWS with GCP services',
-      ],
-      projectsCountText: '3 projects',
-    },
-  ];
+  {
+    name: 'Javascript/TypeScript',
+    blipNumber: '1',
+    heading: 'Javascript/TypeScript',
+    projects: [
+      'Javascript Project with lowercase s',
+      'JavaScript Project with capital S',
+      'Typescript Project',
+      'TypeScript',
+      'AWS Project',
+      'TypeScript with all lowercase',
+      'Javascript with all lowercase',
+    ],
+    projectsCountText: '7 projects',
+  },
+  {
+    name: 'AWS',
+    blipNumber: '2',
+    heading: 'AWS',
+    projects: [
+      'AWS Project',
+      'Another Project - GCP and AWS services',
+      'AWS with GCP services',
+    ],
+    projectsCountText: '3 projects',
+  },
+  {
+    name: 'GCP',
+    blipNumber: '3',
+    heading: 'GCP',
+    projects: [
+      'GCP Project',
+      'Another Project - GCP and AWS services',
+      'AWS with GCP services',
+    ],
+    projectsCountText: '3 projects',
+  },
+];
