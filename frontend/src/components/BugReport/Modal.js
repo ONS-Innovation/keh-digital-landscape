956e88c90dba4f1cb64b5410d065c335dd652d39
sebtheo <Sebastian.Evison-Theodossiades@ons.gov.uk>
2025-09-29 17:13:36 +0100

    feat(bug modal): Sidebar and MenuDropdown open Modal

---
+++ frontend/src/components/BugReport/Modal.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { TbX } from 'react-icons/tb';
+
+const Modal = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+
+  const handleCreateIssue = () => {
+    window.open(
+      'https://github.com/ONS-innovation/keh-digital-landscape/issues/new?labels=bug&template=bug_report.md',
+      '_blank'
+    );
+  };
+
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content" onClick={e => e.stopPropagation()}>
+        <div className="modal-header">
+          <h2>Report a Bug</h2>
+          <button className="modal-close-button" onClick={onClose}>
+            <TbX />
+          </button>
+        </div>
+        <p>
+          You will be redirected to GitHub to create an issue. Please do not
+          include any sensitive information in your bug report.
+        </p>
+        <button className="modal-action-button" onClick={handleCreateIssue}>
+          Continue to GitHub
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
