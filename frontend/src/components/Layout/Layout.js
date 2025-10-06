65fe09d2a68d524719f53e9950acabe5c22ecc1d
sebtheo <Sebastian.Evison-Theodossiades@ons.gov.uk>
2025-10-06 16:32:27 +0100

    fix(Toaster): remove duplicate

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -7,7 +7,25 @@ import { Toaster } from 'react-hot-toast';
 const Layout = ({ children }) => {
   return (
     <div className="layout">
-      <Toaster position="bottom-right" />
+      <Toaster
+        position="bottom-right"
+        reverseOrder={false}
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: 'hsl(var(--background))',
+            opacity: '1',
+            color: 'hsl(var(--foreground))',
+            border: '1px solid hsl(var(--border))',
+            borderRadius: 'var(--radius)',
+            textAlign: 'left',
+            padding: '10px 16px',
+            boxSizing: 'border-box',
+            boxShadow:
+              '0 3px 10px hsl(var(--foreground) / .05), 0 3px 3px hsl(var(--foreground) / .01)',
+          },
+        }}
+      />{' '}
       <Header />
       <div className="layout-content">
         <Sidebar />
