8333eda63df979e3afb38ff40d3f9d9ce0bf729a
Anthony Njonge <47349801+mwirikia@users.noreply.github.com>
2025-10-07 14:26:42 +0100

    update ci.yml and set pipeline

--- concourse/scripts/set_pipeline.sh
+++ concourse/scripts/set_pipeline.sh
@@ -1,30 +1,27 @@
-# repo_name=${1}
 #!/bin/bash
 set -eo pipefail
 # Usage: ./set_pipeline.sh
-# repo_name=$(basename -s .git "$(git config --get remote.origin.url)")
+
+# Define repository name
 repo_name="digital-landscape"
 # Always use the current branch
-branch=$(git rev-parse --abbrev-ref HEAD)
+branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null || { echo "Failed to get branch name"; exit 1; })
 
-if [[ -z "${branch}" ]]; then
-    echo "Branch name is empty, you cannot set a pipeline without a valid branch."
-    exit 1
-fi
-
-# Check that the branch exists
-git rev-parse --verify "${branch}" >/dev/null 2>&1
-if [[ $? -ne 0 ]]; then
-    echo "Branch \"${branch}\" does not exist, you cannot set a pipeline without a valid branch."
+if ! git rev-parse --verify "${branch}" >/dev/null 2>&1; then
+    echo "Branch \"${branch}\" does not exist. Cannot set a pipeline without a valid branch."
     exit 1
 fi
 
 
 if [[ ${branch} == "main" || ${branch} == "master" ]]; then
     tag=$(git tag | tail -n 1)
+    pipeline_name=${repo_name}
 else
     # Remove non-alphanumeric characters and take the first 7 characters
     tag=$(echo "${branch}" | tr -cd '[:alnum:]' | cut -c1-7)
+    pipeline_name=${repo_name}-${branch}
 fi
 
-fly -t aws-sdp set-pipeline -c concourse/ci.yml -p ${repo_name}-${branch}  -v branch=${branch} -v tag=${tag} -v repo_name=${repo_name}
+fly -t aws-sdp set-pipeline -c concourse/ci.yml -p ${pipeline_name}  -v branch=${branch} -v tag=${tag} -v repo_name=${repo_name} -v env=dev
+
+echo "Pipeline \"${pipeline_name}\" set successfully."
