097befecf84b99b71af34986ae6ad3b7e40f26f0
Anthony Njonge <47349801+mwirikia@users.noreply.github.com>
2025-11-12 12:11:56 +0000

    Add bug report email to main UI.

--- frontend/src/components/BugReport/Home.js
+++ frontend/src/components/BugReport/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../styles/components/BugReport.css';
 
 const BugReport = () => {
+  const supportMail = import.meta.env.VITE_SUPPORT_MAIL || 'support_mail@example.org';
   return (
     <div className="bug-report-container">
       <h2>Bug Report</h2>
@@ -16,6 +17,16 @@ const BugReport = () => {
           GitHub
         </a>
         . You will require a GitHub account to open an issue.
+      </span>
+      <p />
+      <span>
+        Or send an email to the {' '}
+        <a
+          href={`mailto:${supportMail}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        > Team
+        </a>.
       </span>
     </div>
   );
