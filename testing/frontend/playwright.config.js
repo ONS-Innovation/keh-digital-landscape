04dd6caa09ea48da7da9a45584f7dc2d59a86623
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2026-01-05 17:01:36 +0000

    fix: extend tests timeout and add more retries
    - This is a bad way to fix the problem
    - Will have to look into improving actions performance

--- testing/frontend/playwright.config.js
+++ testing/frontend/playwright.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'playwright/test';
 export default defineConfig({
   timeout: 60_000, // Boost default timeout to 60 seconds (default is 30 seconds)
   expect: {
-    timeout: 15_000, // Boost default expect timeout to 15 seconds (default is 5 seconds)
+    timeout: 30_000, // Boost default expect timeout to 30 seconds (default is 5 seconds)
   },
-  retries: 1, // Retry failed tests once to account for flakiness
+  retries: 2, // Retry failed tests twice to account for flakiness
 });
