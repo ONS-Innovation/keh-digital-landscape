e6c46e5ccd405249cb67579017a54302a3f874cf
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2026-01-05 17:41:35 +0000

    feat: make config changes only apply in CI
    - CI env var exported in action
    - Check CI value when preparing Playwright config

--- testing/frontend/playwright.config.js
+++ testing/frontend/playwright.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'playwright/test';
 
 export default defineConfig({
-  timeout: 60_000, // Boost default timeout to 60 seconds (default is 30 seconds)
+  timeout: process.env.CI ? 60_000 : 30_000, // Increase default test timeout to 60 seconds on CI (default is 30 seconds)
   expect: {
-    timeout: 30_000, // Boost default expect timeout to 30 seconds (default is 5 seconds)
+    timeout: process.env.CI ? 30_000 : 5_000, // Boost default expect timeout to 30 seconds on CI (default is 5 seconds)
   },
-  retries: 2, // Retry failed tests twice to account for flakiness
+  retries: process.env.CI ? 2 : 0, // Retry failed tests twice on CI to account for flakiness
+
+  workers: process.env.CI ? 1 : undefined, // Limit to 1 worker on CI for stability
 });
