8ec4d1f7bdb8abae09786db194b355637f230532
Anthony Njonge <47349801+mwirikia@users.noreply.github.com>
2026-01-28 16:09:17 +0000

    linting update

--- backend/src/services/alertService.js
+++ backend/src/services/alertService.js
@@ -37,7 +37,11 @@ async function postToWebhook(obj) {
     body: JSON.stringify(obj),
   });
   const text = await resp.text();
-  if (!resp.ok) throw new Error(text || 'Error has occurred during sending an alert message! Please investigate the alert service.');
+  if (!resp.ok)
+    throw new Error(
+      text ||
+        'Error has occurred during sending an alert message! Please investigate the alert service.'
+    );
   return text;
 }
 
